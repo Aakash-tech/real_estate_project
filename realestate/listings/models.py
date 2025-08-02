f134d3a48281a1e38b64fb1ff7f896f4f760fe17
Aakash-tech <akthakur363@gmail.com>
2025-08-02 20:24:01 +0530

    created model for listing and superuser also

--- realestate/listings/models.py
+++ realestate/listings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Property(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    area_sqft = models.FloatField()
+    location = models.CharField(max_length=100)
+    property_type = models.CharField(max_length=50)
+    listed_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
